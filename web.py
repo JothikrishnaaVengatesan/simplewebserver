6d09745e48b36ef419bf175a33ef99fe6cd23d3b
JothikrishnaaVengatesan <vengatesanjothik@gmail.com>
2024-03-13 10:50:55 +0530

    lab

--- web.py
+++ web.py
@@ -10,33 +10,64 @@ content="""
 <caption>Top 5 Revenue Generating Software companies</caption>
 <tr>
 <th>s.no</th>
-<th>companie</th>
+<th>company</th>
 <th>Revenue</th>
+<th>FY</th>
 </tr>
 <tr>
 <th>1</th>
 <th>Microsoft</th>
 <th>65 billion</th>
+<th>2014</th>
 </tr>
 <tr>
 <th>2</th>
 <th>Oracle</th>
 <th>29.6 billion</th>
+<th>2013</th>
 </tr>
 <tr>
 <th>3</th>
-<th>IBMC</th>
-<th>29.1 billion</th>
+<th>SAP</th>
+<th>$20.9</th>
+<th>2013</th>
 </tr>
 <tr>
 <th>4</th>
-<th>SAP</th>
-<th>6.4 billion</th>
+<th>Symantec</th>
+<th>$6.8</th>
+<th>2013</th>
 </tr>
 <tr>
 <th>5</th>
 <th>Symentec</th>
-<th>5.6 billion</th>
+<th>$5.2</th>
+<th>2013</th>
+</tr>
+<th>6</th>
+<th>CA Technologies</th>
+<th>4.7</th>
+<th>2013</th>
+</tr>
+<th>7</th>
+<th>Adobe Systems</th>
+<th>$4.4</th>
+<th>2013</th>
+</tr>
+<th>8</th>
+<th>Fiserv</th>
+<th>$4.5</th>
+<th>2013</th>
+</tr>
+<th>9</th>
+<th>Intuit</th>
+<th>$4.2</th>
+<th>2013</th>
+</tr>
+<th>10</th>
+<th>Amadeus IT Group</th>
+<th>$3.6 </th>
+<th>2013</th>
 </tr>
 </body>
 </html>
